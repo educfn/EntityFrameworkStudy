74354ef25d217c59871126f48a708cb4efdcc6ec
Eduardo CFN <eduardo.cardoso10@gmail.com>
2026-08-10 13:56:23 -0300

    [feat] Implemented first connection - Implemented the first connection.

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ScreenSound.Banco
+{
+    internal class Connection
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Screensound;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
