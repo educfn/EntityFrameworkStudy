96fabcbe33c08652165952204ec9f131b217bcb7
Eduardo C F Neto <eduardo@verion.ind.br>
2026-08-10 15:04:57 -0300

    [fix] Connection - Added the correct parameter to the "Trust Server Certificate".

--- ScreenSound/Banco/Connection.cs
+++ ScreenSound/Banco/Connection.cs
@@ -4,7 +4,7 @@ namespace ScreenSound.Banco
 {
     internal class Connection
     {
-        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Screensound;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Screensound;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
 
         public SqlConnection ObterConexao()
         {
