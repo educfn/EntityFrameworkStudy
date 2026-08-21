263fcd9d933540476d54b6a10dbbb7917f28ef9f
Eduardo CFN <eduardo.cardoso10@gmail.com>
2026-08-20 23:30:21 -0300

    [feat] ORM library - Instalin the Entity.

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco
 {
-    internal class Connection
+    internal class ScreenSoundContext : DbContext
     {
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Screensound;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
 
-        public SqlConnection ObterConexao()
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            return new SqlConnection(connectionString);
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
